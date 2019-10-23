9844febe1a4715529e9820be31a84c761f77310c
darrenhanh@gmail.com <hanh6678909>
2019-10-23 10:24:22 +0800

    发布

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import Components from './components';
+import {elementList, getElementData} from "./constant/commonList";
+import DefaultMenu from './components/DefaultMenu';
+// import Header from './components/Header/index.jsx';
+import TimeLine from './components/TimeLine';
+import FloatTitle from './components/FloatTitle';
+import SimpleLineChart from './components/SimpleLineChart';
+import DefaultTitle from './components/DefalutTitle';
+import SelectOption from "./components/SelectOption";
+import SwitchButton from "./components/SwitchButton";
+import BackgroundHeader from "./components/BackgroundHeader";
+import MultiLineChart from "./components/MultiLineChart";
+import FloatContent from "./components/FloatContent";
+import SimpleBarChart from "./components/SimpleBarChart";
+import {createRandomList} from "./common/dispatch";
+import ImgContent from "./components/ImgContent";
+import SimpleGauge from './components/SimpleGauge';
+import TabsSelect from './components/TabsSelect';
+import {Test} from './components/Test';
+import {PaddingSpan} from "./components/PaddingSpan";
 
-export default Components;
+export  {
+    DefaultMenu, TimeLine, FloatTitle, SimpleLineChart, DefaultTitle, SelectOption, SwitchButton,
+    BackgroundHeader, MultiLineChart, FloatContent, SimpleBarChart, ImgContent, SimpleGauge, TabsSelect, PaddingSpan,
+}
+// export default DefaultMenu;
